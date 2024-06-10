ebea340d5e943ca3a0f58aca26174e2367dd309d
MotuekaHighSchool <williamdk.alt@gmail.com>
2024-06-11 10:30:10 +1200

    initial-commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import Data from "./data.json";
+
+function App() {
+  const [nsn, setNsn] = useState("");
+  const nsnMap = Data.map((student) => student.nsn);
+  const [displayIndex, setDisplayIndex] = useState(-1);
+
+  return (
+    <>
+      <div className="input-group mb-3">
+        <input
+          value={nsn}
+          onChange={(e) => setNsn(e.target.value)}
+          type="text"
+          className="form-control"
+          placeholder="Your NSN"
+          aria-label="Your NSN"
+          aria-describedby="basic-addon2"
+        />
+      </div>
+      {nsn !== "" && <p>Your NSN is {nsn}.</p>}
+
+      {displayIndex >= 0 && <h2>{Data[displayIndex].nsn}</h2>}
+
+      {displayIndex >= 0 && (
+        <p>
+          Your {Data[displayIndex].assessment} assessment is in room{" "}
+          {Data[displayIndex].room}.
+        </p>
+      )}
+
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() =>
+          nsnMap.forEach((element, index) => {
+            if (element == nsn) {
+              console.log(element);
+              setDisplayIndex(index);
+            }
+          })
+        }
+      >
+        Search
+      </button>
+      <button
+        type="button"
+        className="btn btn-danger"
+        onClick={() => {
+          setNsn("");
+          setDisplayIndex(-1);
+        }}
+      >
+        Clear
+      </button>
+    </>
+  );
+}
+export default App;
